29db89e90e23f646c3867a55c7b92fc01ad6da0f
Shlomi Fish <shlomif@shlomifish.org>
2017-06-15 16:33:50 +0300

    add stamp

--- run-range.bash
+++ run-range.bash
@@ -2,4 +2,7 @@ cat ~/freecell-3fc-determine-solvability/vendu-3fc.txt |
 cat | # perl -lanE 'print if /\A29885917\z/ms..1' |
 parallel -j4 --ungroup \
     bash ~/freecell-3fc-determine-solvability/run-single.bash 10000000 "{}" |
-    tee ~/freecell-3fc-determine-solvability/vendu-3fc-output.txt
+    tee -a ~/freecell-3fc-determine-solvability/vendu-3fc-output.txt
+
+touch ~/freecell-3fc-determine-solvability/COMPLETED_STAMP
+echo 'Completed!' 1>&2
