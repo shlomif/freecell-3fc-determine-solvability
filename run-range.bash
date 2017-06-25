1d1b1acbb35e44309238706bedcd7f2d793321df
Shlomi Fish <shlomif@shlomifish.org>
2017-06-25 23:12:15 +0300

    j2

--- run-range.bash
+++ run-range.bash
@@ -1,6 +1,6 @@
 unxz < ~/freecell-3fc-determine-solvability/vendu-2-3fc.txt.xz |
 cat | # perl -lanE 'print if /\A29885917\z/ms..1' |
-parallel -j4 --group --keep-order \
+parallel -j2 --group --keep-order \
     bash ~/freecell-3fc-determine-solvability/run-single.bash 20000000 "{}" |
     tee -a ~/freecell-3fc-determine-solvability/vendu-2-3fc-output.txt
 
