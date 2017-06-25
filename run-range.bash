43b997f7fadd4188c483453d69b34d97c5419126
Shlomi Fish <shlomif@shlomifish.org>
2017-06-25 23:09:48 +0300

    Add the new inputs for vendu.

--- run-range.bash
+++ run-range.bash
@@ -1,8 +1,8 @@
-cat ~/freecell-3fc-determine-solvability/vendu-3fc.txt |
+unxz < ~/freecell-3fc-determine-solvability/vendu-2-3fc.txt.xz |
 cat | # perl -lanE 'print if /\A29885917\z/ms..1' |
 parallel -j4 --group --keep-order \
-    bash ~/freecell-3fc-determine-solvability/run-single.bash 10000000 "{}" |
-    tee -a ~/freecell-3fc-determine-solvability/vendu-3fc-output.txt
+    bash ~/freecell-3fc-determine-solvability/run-single.bash 20000000 "{}" |
+    tee -a ~/freecell-3fc-determine-solvability/vendu-2-3fc-output.txt
 
 touch ~/freecell-3fc-determine-solvability/COMPLETED_STAMP
 echo 'Completed!' 1>&2
