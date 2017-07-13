76f0d1d46dd325a94b728245952884b77c1e0a94
Shlomi Fish <shlomif@shlomifish.org>
2017-07-13 18:49:57 +0300

    New quota for vendu.

--- run-range.bash
+++ run-range.bash
@@ -1,8 +1,8 @@
-unxz < ~/freecell-3fc-determine-solvability/vendu-3-3fc.txt.xz |
+unxz < ~/freecell-3fc-determine-solvability/vendu-4-3fc.txt.xz |
 cat | # perl -lanE 'print if /\A29885917\z/ms..1' |
 parallel -j2 --group --keep-order \
     bash ~/freecell-3fc-determine-solvability/run-single.bash 20000000 "{}" |
-    tee -a ~/freecell-3fc-determine-solvability/vendu-3-3fc-output.txt
+    tee -a ~/freecell-3fc-determine-solvability/vendu-4-3fc-output.txt
 
 touch ~/freecell-3fc-determine-solvability/COMPLETED_STAMP
 echo 'Completed!' 1>&2
