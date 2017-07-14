81155259d6fd450ba3972da0038914b6059c9680
Shlomi Fish <shlomif@shlomifish.org>
2017-07-14 12:50:20 +0300

    add more

--- shlomif-run-range.bash
+++ shlomif-run-range.bash
@@ -1,8 +1,8 @@
 cat /home/shlomif/Download/unpack/games/freecell/freecell-pro-3fc-deals/Int3.txt |
-perl -lanE 'print if ( $_ > 5840251098 )' |
+perl -lanE 'print if ( $_ > 6010597570 )' |
 (
 # lim=12000000
 # lim=8500000
 parallel -j4 --group --keep-order \
     bash ~/Download/unpack/games/freecell/freecell-3fc-determine-solvability/run-single.bash 2400000 "{}"
-) | tee -a ~/Backup/Arcs/freecell-3fc-5e9-intractables.dump.txt
+) | tee -a ~/Backup/Arcs/freecell-3fc-6e9-intractables.dump.txt
