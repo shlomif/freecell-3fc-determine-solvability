968bd31f944d06b700a9cf5b9ba2e5f6c3468914
Shlomi Fish <shlomif@shlomifish.org>
2017-07-23 10:39:34 +0300

    Now can resume the run automatically.
    This is an enhancement to the shlomif-run-range.bash script.

--- shlomif-run-range.bash
+++ shlomif-run-range.bash
@@ -1,8 +1,20 @@
+log_fn="$HOME/Backup/Arcs/freecell-3fc-6e9-intractables.dump.txt"
+if test -e "$log_fn"
+then
+    export START_FROM="$(tail -1 "$log_fn" | perl -lnE 'print $1 if /\A\[\[== End ([0-9]+) ==/')"
+else
+    export START_FROM="-1"
+fi
+if test -z "$START_FROM"
+then
+    echo "Error" 1>&2
+    exit -1
+fi
 cat /home/shlomif/Download/unpack/games/freecell/freecell-pro-3fc-deals/Int3.txt |
-perl -lanE 'print if ( $_ > 6487866417 )' |
+perl -lnE 'BEGIN { $s = int $ENV{START_FROM} ; } print if ( $_ > $s )' |
 (
 # lim=12000000
 # lim=8500000
 parallel -j4 --group --keep-order \
     bash ~/Download/unpack/games/freecell/freecell-3fc-determine-solvability/run-single.bash 2400000 "{}"
-) | tee -a ~/Backup/Arcs/freecell-3fc-6e9-intractables.dump.txt
+) | tee -a "$log_fn"
