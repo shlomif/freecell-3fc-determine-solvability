fb0c91bdd53a1ec431eedad3748dbadcd1c1f040
Shlomi Fish <shlomif@shlomifish.org>
2017-07-24 17:49:38 +0300

    Add the python script for verify+summarize.
    It currently does not do a lot but it caught some deals with no results.

---
+++ summarize1.py
@@ -0,0 +1,45 @@
+import re
+
+def line_iter():
+    FILENAMES = ['freecell-3fc-intractables.dump.txt', 'vendu-3fc-output.txt', 'vendu-2-3fc-output.txt', 'vendu-3-3fc-output.txt', 'vendu-4-3fc-output.txt', 'freecell-3fc-5e9-intractables.dump.txt', 'freecell-3fc-6e9-intractables.dump.txt',]
+    for fn in FILENAMES:
+        with open(fn, 'r') as fh:
+            for l in fh:
+                yield l.rstrip('\r\n')
+
+def deal_iter():
+    it = line_iter()
+    l = it.next()
+    while l:
+        lines = [l]
+        l = it.next()
+        while l is not None and not l.startswith('[[== End '):
+            lines.append(l)
+            l = it.next()
+        lines.append(l)
+        idx1 = int(re.match(r'^== ([0-9]+) ==$', lines[0]).group(1))
+        idx2 = int(re.match(r'^\[\[== End ([0-9]+) ==\]\]$', lines[-1]).group(1))
+        if idx1 != idx2:
+            raise BaseException("index mismatch - %d ; %d" % (idx1,idx2))
+        verdict = None
+        count = -1
+        if len([x for x in lines if x.startswith("I could not solve this game.")]) > 0:
+            verdict = "impossible"
+        elif len([x for x in lines if x.startswith("This game is solv")]) > 0:
+            verdict = "solved"
+        elif len([x for x in lines if x.startswith("Iterations count exceeded")]) > 0:
+            verdict = "intract"
+            for x in lines:
+                m = re.match(r'^Total number of states checked is ([0-9]+)\.', x)
+                if m:
+                    count = int(m.group(1))
+                    break
+        else:
+            print(lines)
+            # raise BaseException("Invalid state - %d" % (idx1))
+            print("Invalid state - %d" % (idx1))
+        yield {'idx': idx1, 'verdict': verdict, 'count': count}
+        l = it.next()
+
+for d in deal_iter():
+    pass # print(d)
