b8aaf846dc27635820528974d453945d4c90f4db
Shlomi Fish <shlomif@shlomifish.org>
2017-07-26 19:42:28 +0300

    New range for vendu + resumable script.

--- run-range.bash
+++ run-range.bash
@@ -1,8 +1,25 @@
-unxz < ~/freecell-3fc-determine-solvability/vendu-4-3fc.txt.xz |
-perl -lnE 'print if $_ > 4822584186 ' |
-parallel -j2 --group --keep-order \
-    bash ~/freecell-3fc-determine-solvability/run-single.bash 20000000 "{}" |
-    tee -a ~/freecell-3fc-determine-solvability/vendu-4-3fc-output.txt
+log_fn="$HOME/freecell-3fc-determine-solvability/vendu-5-3fc-output.txt"
 
+if test -e "$log_fn"
+then
+    export START_FROM="$(tail -1 "$log_fn" | perl -lnE 'print $1 if /\A\[\[== End ([0-9]+) ==/')"
+else
+    export START_FROM="-1"
+fi
+
+if test -z "$START_FROM"
+then
+    echo "Error" 1>&2
+    exit -1
+fi
+
+unxz < ~/freecell-3fc-determine-solvability/vendu-5-3fc.txt.xz |
+perl -lnE 'BEGIN { $s = int $ENV{START_FROM} ; } print if ( $_ > $s )' |
+(
+# lim=12000000
+# lim=8500000
+parallel -j2 --group --keep-order \
+    bash ~/freecell-3fc-determine-solvability/run-single.bash 20000000 "{}"
+) | tee -a "$log_fn"
 touch ~/freecell-3fc-determine-solvability/COMPLETED_STAMP
 echo 'Completed!' 1>&2
