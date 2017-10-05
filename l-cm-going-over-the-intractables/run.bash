ea14d4a5eb3602e893057cfcd5858cbfb1e7976a
Shlomi Fish <shlomif@shlomifish.org>
2017-10-05 11:21:23 +0300

    Scripts and results from running the cm preset.
    On the intractables - it was kinda slow and barely made progress.

---
+++ l-cm-going-over-the-intractables/run.bash
@@ -0,0 +1,1 @@
+./summary-fc-solve `cat ~/intract | head -32000` -- -l cm -mi 20000000 | tee -a ~/s
---
+++ l-cm-going-over-the-intractables/tatzer.bash
@@ -0,0 +1,1 @@
+../source/Tatzer -l n2b --prefix="/home/shlomif/apps/fcs"  -l extra_speed  --disable-ni --without-help --without-iter-handler --rcs -nfc=3 --no-disable-moves-track --no-without-max-num-states
