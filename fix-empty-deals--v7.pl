eb9e1a85188395a9f479e98318ab14864186ab35
Shlomi Fish <shlomif@shlomifish.org>
2017-10-25 13:09:00 +0300

    Add more.

--- fix-empty-deals--v7.pl
+++ fix-empty-deals--v7.pl
@@ -2,5 +2,5 @@ s#(\n== ([0-9]+) ==\n\[\[== End ([0-9]+) ==\]\]\n)#
 my ($t, $i, $i2) = ($1, $2, $3);
 ($i eq $i2) ? do {
 STDERR->print("Handling $i\n");
-"\n".`bash ~/Download/unpack/games/freecell/freecell-3fc-determine-solvability/run-single.bash 8000000 $i` ; } : $t
+"\n".`bash ~/Download/unpack/games/freecell/freecell-3fc-determine-solvability/run-single.bash "\${LIM:-20000000}" $i` ; } : $t
 #egms;
