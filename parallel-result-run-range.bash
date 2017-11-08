486d206f73f84250b94b22fc739beeb44fb47451
Shlomi Fish <shlomif@shlomifish.org>
2017-11-08 02:57:36 +0200

    some improvements to the scripts

--- parallel-result-run-range.bash
+++ parallel-result-run-range.bash
@@ -1,10 +1,10 @@
-log_fn="$HOME/freecell-3fc-determine-solvability/vendu-10-3fc-output.txt"
+log_fn="$HOME/aws-3-3fc-output.txt"
 
 if test -e "$log_fn"
 then
     export START_FROM="$(tail -1 "$log_fn" | perl -lnE 'print $1 if /\A\[\[== End ([0-9]+) ==/')"
 else
-    export START_FROM="-1"
+    export START_FROM="6499999999"
 fi
 
 if test -z "$START_FROM"
@@ -14,14 +14,13 @@ then
 fi
 
 export FREECELL_SOLVER_QUIET=1
-ulimit -n 10000
-unxz < ~/freecell-3fc-determine-solvability/vendu-10-3fc.txt.xz |
+unxz < ~/vendu-10-3fc.txt.xz |
 perl -lnE 'BEGIN { $s = int $ENV{START_FROM} ; } print if ( $_ > $s )' |
 (
 # lim=12000000
 # lim=8500000
-parallel -j2 --group --keep-order \
-    bash ~/freecell-3fc-determine-solvability/run-single.bash 20000000 "{}"
-) | tee -a "$log_fn"
+parallel -j7 --group --result "$HOME/RES" \
+    bash ~/run-single.bash 20000000 "{}"
+)
 touch ~/freecell-3fc-determine-solvability/COMPLETED_STAMP
 echo 'Completed!' 1>&2
