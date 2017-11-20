e28c91250079af8cff6b2f58a1671ce77410aa3b
Shlomi Fish <shlomif@shlomifish.org>
2017-11-20 22:22:28 +0200

    Conver to $HOME.

--- filter-aws-patsolve-deals.pl
+++ filter-aws-patsolve-deals.pl
@@ -7,7 +7,7 @@ use IO::All qw/ io /;
 use Path::Tiny qw/ path /;
 
 my %solved = map { $_ => 1 } map { /\A#([0-9]+) - Won\z/ ? $1 : die} grep /- Won/,
-io->file("/home/shlomif/Download/unpack/games/freecell/freecell-3fc-determine-solvability/aws-patsolve/run-dump-4.txt")->chomp->getlines;
+io->file("$ENV{HOME}/Download/unpack/games/freecell/freecell-3fc-determine-solvability/aws-patsolve/run-dump-4.txt")->chomp->getlines;
 
 
 path("Int3.txt")->edit_lines_utf8(sub { $_ = '' if /([0-9]+)/ and exists$solved{$1}});
--- filter-summary-Solved-deals.pl
+++ filter-summary-Solved-deals.pl
@@ -7,6 +7,6 @@ use IO::All qw/ io /;
 use Path::Tiny qw/ path /;
 
 my %solved = map { $_ => 1 } map { /\A([0-9]+) =/ ? $1 : die} grep /Solved/,
-io->file("/home/shlomif/fc3-int3-results.txt")->chomp->getlines;
+io->file("$ENV{HOME}/Download/unpack/games/freecell/freecell-3fc-determine-solvability/l-cm-going-over-the-intractables/fc3-int3-results.txt")->chomp->getlines;
 
 path("Int3.txt")->edit_lines_utf8(sub { $_ = '' if /([0-9]+)/ and exists$solved{$1}});
