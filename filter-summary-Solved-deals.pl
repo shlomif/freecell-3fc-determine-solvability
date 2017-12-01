659ddadbe49aaaa77999c6d2b1d50d37020dcdf0
Shlomi Fish <shlomif@shlomifish.org>
2017-12-01 19:47:44 +0200

    Add the deals as ran on vendu's machine.

--- filter-summary-Solved-deals.pl
+++ filter-summary-Solved-deals.pl
@@ -7,6 +7,6 @@ use IO::All qw/ io /;
 use Path::Tiny qw/ path /;
 
 my %solved = map { $_ => 1 } map { /\A([0-9]+) =/ ? $1 : die} grep /Solved/,
-io->file("$ENV{HOME}/Download/unpack/games/freecell/freecell-3fc-determine-solvability/l-cm-going-over-the-intractables/fc3-int3-results.txt")->chomp->getlines;
+map { io->file("$ENV{HOME}/Download/unpack/games/freecell/freecell-3fc-determine-solvability/l-cm-going-over-the-intractables/$_")->chomp->getlines } (qw# fc3-int3-results.txt vendu-20-3fc-output.txt #);
 
 path("Int3.txt")->edit_lines_utf8(sub { $_ = '' if /([0-9]+)/ and exists$solved{$1}});
