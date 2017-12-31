129276510499d4cce8d90a4be6198b1c947343d1
Shlomi Fish <shlomif@shlomifish.org>
2017-12-31 11:03:38 +0200

    update the script

--- filter-summary-Solved-deals.pl
+++ filter-summary-Solved-deals.pl
@@ -7,6 +7,6 @@ use IO::All qw/ io /;
 use Path::Tiny qw/ path /;
 
 my %solved = map { $_ => 1 } map { /\A([0-9]+) =/ ? $1 : die} grep /Solved/,
-map { io->file("$ENV{HOME}/Download/unpack/games/freecell/freecell-3fc-determine-solvability/l-cm-going-over-the-intractables/$_")->chomp->getlines } (qw# fc3-int3-results.txt fc3-int3-results--2.txt vendu-20-3fc-output.txt #);
+map { io->file("$ENV{HOME}/Download/unpack/games/freecell/freecell-3fc-determine-solvability/l-cm-going-over-the-intractables/$_")->chomp->getlines } (qw# fc3-int3-results.txt fc3-int3-results--2.txt fc3-int3-results--3.txt vendu-20-3fc-output.txt #);
 
 path("Int3.txt")->edit_lines_utf8(sub { $_ = '' if /([0-9]+)/ and exists$solved{$1}});
