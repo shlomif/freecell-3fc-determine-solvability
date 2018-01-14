a217f051396153f556d725bfb4a1bb0e5e959f4d
Shlomi Fish <shlomif@shlomifish.org>
2018-01-14 19:17:11 +0200

    tidy

--- filter-summary-Solved-deals.pl
+++ filter-summary-Solved-deals.pl
@@ -6,7 +6,20 @@ use warnings;
 use IO::All qw/ io /;
 use Path::Tiny qw/ path /;
 
-my %solved = map { $_ => 1 } map { /\A([0-9]+) =/ ? $1 : die} grep /Solved/,
-map { io->file("$ENV{HOME}/Download/unpack/games/freecell/freecell-3fc-determine-solvability/l-cm-going-over-the-intractables/$_")->chomp->getlines } (qw# fc3-int3-results.txt fc3-int3-results--2.txt fc3-int3-results--3.txt fc3-int3-results--4.txt vendu-20-3fc-output.txt #);
+my %solved = map { $_ => 1 } map { /\A([0-9]+) =/ ? $1 : die } grep /Solved/,
+    map {
+    io->file(
+"$ENV{HOME}/Download/unpack/games/freecell/freecell-3fc-determine-solvability/l-cm-going-over-the-intractables/$_"
+        )->chomp->getlines
+    } (
+    qw#
+        fc3-int3-results.txt
+        fc3-int3-results--2.txt
+        fc3-int3-results--3.txt
+        fc3-int3-results--4.txt
+        vendu-20-3fc-output.txt
+        #
+    );
 
-path("Int3.txt")->edit_lines_utf8(sub { $_ = '' if /([0-9]+)/ and exists$solved{$1}});
+path("Int3.txt")
+    ->edit_lines_utf8( sub { $_ = '' if /([0-9]+)/ and exists $solved{$1} } );
