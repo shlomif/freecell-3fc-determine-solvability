a4c494f619be6b30bdc727e6c0df0e81767e73b8
Shlomi Fish <shlomif@shlomifish.org>
2018-01-14 19:08:46 +0200

    Update the range for the new run.

--- shlomif-run-range--2.bash
+++ shlomif-run-range--2.bash
@@ -1,11 +1,11 @@
 cd ~/Backup/Arcs/fc-solve/3fc-b/
-log_fn="/home/shlomif/Arcs/fc3-int3-results--3.txt"
+log_fn="/home/shlomif/Arcs/fc3-int3-results--4.txt"
 
 if test -e "$log_fn"
 then
     export START_FROM="$(tail -1 "$log_fn" | perl -lnE 'print $1 if /\A([0-9]+) =/')"
 else
-    export START_FROM="2000000000"
+    export START_FROM="1083741929"
 fi
 
 if test -z "$START_FROM"
@@ -15,7 +15,7 @@ then
 fi
 
 export FREECELL_SOLVER_QUIET=1
-./summary-fc-solve slurp <(perl -lnE 'BEGIN { $s = int $ENV{START_FROM} ; } print if ( $_ > $s and $_ < 3e9 )' < ~/Download/unpack/games/freecell/freecell-pro-3fc-deals--split/Int3.txt ) -- -l cm -mi 20000000 |
+./summary-fc-solve slurp <(perl -lnE 'BEGIN { $s = int $ENV{START_FROM} ; } print if ( $_ > $s and $_ < 2e9 )' < ~/Download/unpack/games/freecell/freecell-pro-3fc-deals--split/Int3.txt ) -- -l cm -mi 20000000 |
     tee -a "$log_fn" |
     commify |
     timestamper
