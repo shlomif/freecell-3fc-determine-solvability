52053a81109c4e9f9d75b22ed8bbebc9fe907a1c
Shlomi Fish <shlomif@shlomifish.org>
2018-02-03 13:25:42 +0200

    Filter more based on the latest scans.

--- filter-summary-Solved-deals.pl
+++ filter-summary-Solved-deals.pl
@@ -6,11 +6,11 @@ use warnings;
 use IO::All qw/ io /;
 use Path::Tiny qw/ path /;
 
+my $REPO_PATH =
+"$ENV{HOME}/Download/unpack/games/freecell/freecell-3fc-determine-solvability";
 my %solved = map { $_ => 1 } map { /\A([0-9]+) =/ ? $1 : die } grep /Solved/,
     map {
-    io->file(
-"$ENV{HOME}/Download/unpack/games/freecell/freecell-3fc-determine-solvability/l-cm-going-over-the-intractables/$_"
-        )->chomp->getlines
+    io->file("$REPO_PATH/l-cm-going-over-the-intractables/$_")->chomp->getlines
     } (
     qw#
         fc3-int3-results.txt
@@ -20,6 +20,22 @@ my %solved = map { $_ => 1 } map { /\A([0-9]+) =/ ? $1 : die } grep /Solved/,
         vendu-20-3fc-output.txt
         #
     );
+
+foreach my $l (
+    io->file(
+"$REPO_PATH/try-to-find-random-dfs-seeds-that-solve-ranges-of-deals/3e9-to-4e9/seeds.log.txt"
+    )->chomp->getlines
+    )
+{
+    if ( my ($deal) = $l =~ /\AFound seed = [0-9]+ for deal = ([0-9]+)\z/ )
+    {
+        $solved{$deal} = 1;
+    }
+    else
+    {
+        die "wrong line '$l'!";
+    }
+}
 
 path("Int3.txt")
     ->edit_lines_utf8( sub { $_ = '' if /([0-9]+)/ and exists $solved{$1} } );
